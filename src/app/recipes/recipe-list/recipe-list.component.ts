d04f32a738027e28908c6301335204dffbb423c4
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-09 00:18:54 +0530

    61

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800'
+    ),
+  ];
+
+  constructor() {}
+}
