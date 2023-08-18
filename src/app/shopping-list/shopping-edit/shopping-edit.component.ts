e0cad2573f65fa5c582390d54ab43e5a71b9b996
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-18 23:43:50 +0530

    090

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef | undefined;
+  @ViewChild('amountInput') amountInputRef: ElementRef | undefined;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {}
 
+  onAddItem() {
+    const ingName = this.nameInputRef?.nativeElement.value;
+    const ingAmount = this.amountInputRef?.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
