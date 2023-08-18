5ad085755266ac92ef87ca07937e5018fdc7d74b
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-18 23:22:31 +0530

    089

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -14,11 +16,16 @@ export class RecipeListComponent {
       'https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800'
     ),
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
+      'A Test Recipe 2',
+      'This is simply a test 2',
       'https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800'
     ),
   ];
 
   constructor() {}
+
+  onRecipeSelected(eve: Recipe) {
+    // console.log(eve);
+    this.recipeWasSelected.emit(eve);
+  }
 }
